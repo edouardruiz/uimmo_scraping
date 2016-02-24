87bb56b37a7bafbda29d128217a482138d0947fb
Edouard Ruiz <edouard.ruiz-ext@sgcib.com>
2016-02-24 20:22:17 +0100

    First scrap.

--- immo_scraping/scraping.py
+++ immo_scraping/scraping.py
@@ -1,8 +1,18 @@
+import requests
+
 from bs4 import BeautifulSoup
 
 
+URL_SE_LOGER = r"http://www.seloger.com/list.htm?ci=750116&idtt=2"
+
+proxies = {
+  'http': 'http://edouard.ruiz:hedv1084@proxy.int.world.socgen:8080',
+  'https': 'http://edouard.ruiz:hedv1084@proxy.int.world.socgen:8080',
+}
+
+
 def main():
-    pass
+    se_loger = requests.get(URL_SE_LOGER, proxies=proxies)
 
 
 if __name__ == '__main__':
