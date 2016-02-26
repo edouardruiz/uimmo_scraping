5b50852ef4127224b1a45fb100f30479b7cc7b7a
Edouard Ruiz <edouard.ruiz@gmail.com>
2016-02-26 12:52:20 +0100

    Remove login password info. Split scrapping in functions.

--- immo_scraping/scraping.py
+++ immo_scraping/scraping.py
@@ -1,27 +1,58 @@
 import os
+import sys
+import getpass
 import requests
 
 from bs4 import BeautifulSoup
 
 
-URL_SE_LOGER = r"http://www.seloger.com/list.htm?ci=750116&idtt=2"
+URL_SE_LOGER_SEARCH = r"http://www.seloger.com/list.htm?ci=750116&idtt=2&idtypebien=1"
+URL_SE_LOGER_AD = r"http://www.seloger.com/annonces/achat/appartement/paris-18eme-75/la-chapelle-marx-dormoy/106474505.htm?cp=75018&idtt=2&org=advanced_search&bd=Li_LienAnn_1"
 
-proxies = {
-  'http': 'http://edouard.ruiz:hedv1084@proxy.int.world.socgen:8080',
-  'https': 'http://edouard.ruiz:hedv1084@proxy.int.world.socgen:8080',
+PROXIES = {
+  'http': 'http://{}:{}@proxy.int.world.socgen:8080',
+  'https': 'http://{}:{}@proxy.int.world.socgen:8080',
 }
 
 
-def main():
-    if os.environ.get('USE_PROXY', True):
-        se_loger = requests.get(URL_SE_LOGER, proxies=proxies)
+def build_article_json(article):
+    return {
+        'title': ''.join(article.h2.a.strings),
+        'price': ''.join(article.find('div', 'price').a.strings),
+        'link': article.a['href']
+    }
+
+
+def build_flat_list(soup):
+    return list(map(build_article_json, soup.body.find_all('article')))
+
+
+def get_proxies():
+    if sys.stdin.isatty():
+        l = input('Login: ')
+        p = getpass.getpass()
     else:
-        se_loger = requests.get(URL_SE_LOGER)
+        l = sys.stdin.readline().rstrip()
+        p = sys.stdin.readline().rstrip()
 
-    soup = BeautifulSoup(se_loger.content)
+    PROXIES['http'] = PROXIES['http'].format(l, p)
+    PROXIES['https'] = PROXIES['https'].format(l, p)
 
-    print(soup.prettify())
+    return PROXIES
+
+
+def main():
+    if os.environ.get('USE_PROXY', False) == 'True':
+        se_loger = requests.get(URL_SE_LOGER_SEARCH, proxies=get_proxies())
+    else:
+        se_loger = requests.get(URL_SE_LOGER_SEARCH)
+
+    soup = BeautifulSoup(se_loger.content, 'html.parser')
+    articles = build_flat_list(soup)
+
+    print(articles)
 
 
 if __name__ == '__main__':
-    main()
+    get_proxies()
+    # main()
